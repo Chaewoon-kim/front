abdcad01697ba45c49e2a78af37e83314c2ceca0
[Github <[Github>
2024-07-08 14:49:03 +0900

    js file:   20240708/06-3.js 	new file:   20240708/Basic.html     js 필기:   20240708/JavaScript.txt 	new file:   20240708/ex06-3.html

---
+++ 20240708/06-3.js
@@ -0,0 +1,9 @@
+//함수 정의 function 키워드 
+//1. over() 함수 -> 이미지 변환
+function over(obj){
+    obj.src = "../images/banana.jpg";
+}
+// 2. out() 함수 -> 마우스에서 내려놨을 때 원래 이미지로 변환
+function out(obj){
+    obj.src = "../images/apple.jpg";
+}
